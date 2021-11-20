c54a05e15be353a65ef83c667ce5c19c1dce342b
your git username <your git email>
2021-11-19 18:45:21 -0600

    added image cropping

--- src/schemas/image.schema.js
+++ src/schemas/image.schema.js
@@ -4,4 +4,14 @@ exports.imageSchema = Joi.object({
   url: Joi.string().uri().required(),
   name: Joi.string().required(),
   path: Joi.string().allow("").required(),
+  crop: Joi.object({
+    x: Joi.number()
+      .description("x coordinate of the left corner of the cropped area.")
+      .required(),
+    y: Joi.number()
+      .description("y coordinate of the top corner of the cropped area.")
+      .required(),
+    width: Joi.number().description("width of the cropped area.").required(),
+    height: Joi.number().description("height of the cropped area.").required(),
+  }).optional(),
 }).allow(null);
